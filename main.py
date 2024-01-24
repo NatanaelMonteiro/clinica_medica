755f642dcf1f3cd5c98685f1aca550e93024b220
Natanael Monteiro Lima <dr.natanael.lima@gmail.com>
2024-01-24 13:18:21 -0300

    Servir static pages pela FastApi

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
+app.mount("/app", StaticFiles(directory="static", html="true"), name="static")
 
-@app.get("/")
+@app.get("/", response_class=RedirectResponse)
+async def root():
+    return "/app/login.html"
 
-async def home():
-    
-    return "Funcionou"
-    
+@app.get("/api")
+async def api():
+    return {"dados": "Lista de medicos"}
