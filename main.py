66034a9827b6e2964dcf6b33c69b2b9f7484ff48
Natanael Monteiro Lima <dr.natanael.lima@gmail.com>
2024-01-25 12:13:38 -0300

    Add redirect to login page

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, HTMLResponse
 
 app = FastAPI()
 app.mount("/app", StaticFiles(directory="static", html="true"), name="static")
+
 
 @app.get("/", response_class=RedirectResponse)
 async def root():
     return "/app/login.html"
 
-@app.get("/api")
+
+@app.get("/api", response_class=HTMLResponse)
 async def api():
-    return {"dados": "Lista de medicos"}
+    return """
+    <!DOCTYPE html>
+    <html>
+        <head>
+             <meta http-equiv="refresh" content="0; URL='./app/login.html'">
+        </head>
+    </html>
+    """
