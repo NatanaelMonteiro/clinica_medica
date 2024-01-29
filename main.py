76a93dcf615d8f8e28bc3856ff87b75114c16845
Natanael Monteiro Lima <dr.natanael.lima@gmail.com>
2024-01-29 13:47:52 -0300

    Exibir dados usando template jinja2

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 app.mount("/app", StaticFiles(directory="static", html="true"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=RedirectResponse)
@@ -11,28 +13,69 @@ async def root():
     return "/app/login.html"
 
 
-@app.get("/api", response_class=HTMLResponse)
+@app.get("/api", response_class=RedirectResponse)
 async def api():
-    return """
-    <!DOCTYPE html>
-    <html>
-        <head>
-             <meta http-equiv="refresh" content="0; URL='./app/login.html'">
-        </head>
-    </html>
-    """
+    return "/app/login.html"
 
-@app.get("/api/medicos")
-async def medico():
-    return get_medicos()
+
+@app.get("/api/medicos", response_class=HTMLResponse)
+async def medicos(request: Request):
+    medicos = get_medicos()
+    dados_html= templates.TemplateResponse("tpl_medicos.html", {"request": request, "dados": medicos})
+                                           
+    return dados_html
+
+
+# @app.get("/api/medicos", response_class=HTMLResponse)
+# async def medicos():
+#     dados_html = """
+#         <table>
+#             <thead>
+#                 <tr>
+#                     <th>Nome</th>
+#                     <th>RCM</th>
+#                     <th>Especialidade</th>
+#                     <th>Turno</th>
+#                     <th>Situação</th>
+#                     <th colspam="2">&nbsp;</th>
+#                 </tr>
+#             </thead>
+#             <tbody>
+#                 <tr>
+#                     <td>Rancho Crux</td>
+#                     <td>0001</td>
+#                     <td>Traumatologia</td>
+#                     <td>Noturno</td>
+#                     <td>Ativo</td>
+#                     <td>
+#                         <i class="fa fa-pencil"></i>&nbsp;
+#                     </td>
+#                     <td>
+#                         <i class="fa fa-trash-o"></i>
+#                     </td>
+#                 </tr>
+#             </tbody>
+#         </table>
+#         """
+
+#     return dados_html
 
 def get_medicos():
-    dados = {
-        "nome": "Rancho Crux",
-        "crm": "0001" ,
-        "especialidade": "Traumatologia",
-        "turno": "Noturno",
-        "situação": "Ativo",
-    }
+    dados = [
+        {
+            "nome": "Rancho Crux",
+            "crm": "0001",
+            "especialidade": "Traumatologia",
+            "turno": "Noturno",
+            "situação": "Ativo",
+        },
+        {
+            "nome": "Álvaro Cortez",
+            "crm": "0002",
+            "especialidade": "Radiologia",
+            "turno": "Vespertino",
+            "situação": "Ativo",
+        },
+    ]
 
     return dados
