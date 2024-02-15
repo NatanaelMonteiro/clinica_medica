1cbf6ef38c3c4dab0b90eddc0674c9b91a75dea0
Natanael Monteiro Lima <dr.natanael.lima@gmail.com>
2024-02-15 16:09:21 -0300

    Adicionado modal de confirmação animado

---
+++ static/js/modal.js
@@ -0,0 +1,21 @@
+document.addEventListener(
+    "htmx:confirm",
+    function (e) {
+        if (e.detail.question !== null) {
+            e.preventDefault();
+            Swal.fire({
+                // icon: "question",
+                buttonsStyling: false,
+                showCancelButton: true,
+                reverseButtons: true,
+                title: "Atenção!!!",
+                text: `Tem certeza que deseja excluir o cadastro de ${(e.detail.question).toUpperCase()}?`,
+                showClass: { popup: "animate__animated animate__fadeInUp animate__faster" },
+                hideClass: { popup: "animate__animated animate__fadeOutDown animate__faster" },
+            }).then(function (resultado) {
+                if (resultado.isConfirmed) e.detail.issueRequest(true)
+            })
+
+        }
+    }
+)
