00dceb2a0bbecdc249a51073859118ef7a7c8334
Natanael Monteiro Lima <dr.natanael.lima@gmail.com>
2024-02-21 13:37:47 -0300

    Ajuste de layout das telas

--- static/fragments/html_edit.py
+++ static/fragments/html_edit.py
@@ -5,23 +5,23 @@ def paciente_html(dados):
             <td><input name="email" value="{dados['email']}"></td>
             <td><input name="telefone" value="{dados['telefone']}"></td>
             <td><input name="status" value="{dados['status']}"></td>
-            <td>
-                <a hx-get="/api/pacientes"
-                    title="Cancelar a alteração"
-                    hx-swap="outerHTML" 
-                    hx-target="closest table">
-                    <i class="material-symbols-outlined x2">undo</i>
-                </a>
-            </td>
-            <td>
-                <a hx-trigger="click" 
-                    hx-include="closest tr"
-                    hx-put="/api/pacientes/{dados['id']}" 
-                    title="Salvar"
-                    hx-swap="outerHTML" 
-                    hx-target="closest table">
-                    <i class="material-symbols-outlined x2">save</i>
-                </a>
+            <td class="icon">
+                <div>
+                    <a hx-get="/api/pacientes"
+                        title="Cancelar a alteração"
+                        hx-swap="outerHTML" 
+                        hx-target="closest table">
+                        <i class="material-symbols-outlined x2">undo</i>
+                    </a>
+                    <a hx-trigger="click" 
+                        hx-include="closest tr"
+                        hx-put="/api/pacientes/{dados['id']}" 
+                        title="Salvar"
+                        hx-swap="outerHTML" 
+                        hx-target="closest table">
+                        <i class="material-symbols-outlined x2">save</i>
+                    </a>
+                </div>
             </td>
         </tr>
         """
